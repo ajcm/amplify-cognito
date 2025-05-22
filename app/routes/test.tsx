32dd108b89abd14b7aa682e65c2b9a7437f9c30e
ajcm <ajcm2020@proton.me>
2025-05-22 21:53:56 +0100

    xpto

---
+++ app/routes/test.tsx
@@ -0,0 +1,18 @@
+
+
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+        <header className="flex flex-col items-center gap-9">
+         
+        </header>
+        <div className="max-w-[300px] w-full space-y-6 px-4">
+         <b>Test ......................</b>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+
